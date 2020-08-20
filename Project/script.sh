8697c6d5c39a652cd044427d4fd494e89e529299
akhilraj939 <65364447+akhilraj939@users.noreply.github.com>
2020-08-20 13:08:53 -0400

    Update script.sh

--- Project/script.sh
+++ Project/script.sh
@@ -7,5 +7,7 @@ git clone https://github.com/akhilraj939/Learnings.git
 cd Learnings/Project
 
 sudo ansible-playbook updateplay.yml
+sudo sudo cp -i /etc/kubernetes/admin.conf /home/akhil/.kube/config
+sudo chown $(id -u):$(id -g) 
 sudo kubectl get -n kube-system pods >> akhil
 cat akhil
